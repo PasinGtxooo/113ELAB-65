b98cafa81ac1c590d44f8bac03e7cbbff5b1ce91
Gtxo <mos.pasin2547@gmail.com>
2023-02-12 15:30:26 +0700

    LAB7

---
+++ lab7/l7-7.c
@@ -0,0 +1,19 @@
+ให้คืนตำแหน่งหน่วยความจำของโปรแกรม (ที่ได้ให้ pointer ใช้งานเสร็จแล้ว) กลับสู่ระบบ
+
+โดยกำหนดการใช้งานหน่วยความจำดังนี้
+
+
+float **ptr;
+int i,m,n;
+ptr=(float**)malloc(m*sizeof(float*));
+for(i=0;i<m;i++)
+    ptr[i]=(float*)malloc(n*sizeof(float));
+/* Do something with ptr */
+
+/*......Ans1......*/
+/*......Ans2......*/
+/*......Ans3......*/
+
+Ans1  = "for(i=0;i<m;i++)";
+Ans2  = "free(ptr[i]);";
+Ans3  = "free(ptr);";
